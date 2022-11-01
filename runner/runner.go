244eebc63e2f4edfe8b18b4353a79c02645616d5
Said Neder <snederc@protonmail.com>
2022-11-01 12:35:57 -0500

    DarKali testing

--- runner/runner.go
+++ runner/runner.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"os/exec"
+	"time"
 
 	"github.com/zakaria-chahboun/cute"
 )
@@ -10,10 +11,14 @@ func Run(cmd *exec.Cmd) {
 	cute.SetTitleColor(cute.BrightBlue)
 	cute.SetMessageColor(cute.BrightGreen)
 	cute.Println("Running script, please don't kill me...")
-	err := cmd.Run()
+	err := cmd.Start()
 	if err != nil {
-		cute.Check("Err: ", err)
-	} else {
-		cute.Println("Done!")
+		cute.Check("Error: ", err)
+	}
+	time.Sleep(8 * time.Second)
+	cute.Println("Wating the script to complete...")
+	err = cmd.Wait()
+	if err != nil {
+		cute.Check("Error: ", err)
 	}
 }
