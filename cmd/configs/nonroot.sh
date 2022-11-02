f829fd7f6a9e18db52cb2b1f9e3d46957d9c2ae5
Said Neder <snederc@protonmail.com>
2022-11-02 11:06:09 -0500

    testing

--- cmd/configs/nonroot.sh
+++ cmd/configs/nonroot.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo 'export PATH="/home/$(whoami)/.local/bin:$PATH"' >> ~/.zshrc
-sleep 2
 cp -r ./cmd/configs/dark_kali_dots/* ~/.config
-sleep 2
 pip install psutil
-sleep 2
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-sleep 2
 ./cmd/configs/nerdy.sh Hack
-sleep 2
--- cmd/configs/system.sh
+++ cmd/configs/system.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 apt update -yy && apt upgrade -yy
-sleep 2
 apt install kali-desktop-core git neovim python3 python3-pip libpangocairo-1.0-0 alsa-utils pavucontrol thunar suckless-tools lightdm kitty nitrogen picom golang-go neofetch rofi xorg xserver-xorg xinit python3-pip python3-xcffib python3-cairocffi keepassxc feh -yy
-sleep 2
 pip install qtile
-sleep 2
 cp ./cmd/configs/qtile.desktop /usr/share/xsessions
-sleep 2
 mv /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-config-original.conf
-sleep 1
 cp ./cmd/configs/lightdm-gtk-greeter.conf /etc/lightdm/
-sleep 2
 systemctl enable lightdm
-sleep 2
 distro=$(if echo " bullseye focal impish jammy uma una vanessa" | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
 tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
@@ -26,4 +19,3 @@ EOF
 apt update
 apt install librewolf -y
 apt remove qterminal
-sleep 2
