0822225cbe38f24a8103067491f97eacd83326b1
Said Neder <snederc@protonmail.com>
2022-11-02 11:06:58 -0500

    testing

--- cmd/configs/nonroot.sh
+++ cmd/configs/nonroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'export PATH="/home/$(whoami)/.local/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/home/$(whoami)/.local/bin:/home/$(whoami)/go/bin:$PATH"' >> ~/.zshrc
 cp -r ./cmd/configs/dark_kali_dots/* ~/.config
 pip install psutil
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
